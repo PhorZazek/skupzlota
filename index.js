c5a334baf77e412ba1550648d7cb5bdef60401c8
PhorZazek <adversary.jj@gmail.com>
2026-03-08 12:52:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+async function getPrice(){
+    try{
+        const response = await fetch("https://phorzazek.pythonanywhere.com/gold_price/");
+    
+        if (!response.ok){
+            throw new Error("HTTP error: " + response.status);
+        }
+        const data = await response.json();
+
+        console.log(data.cena);
+        document.getElementById("price333").innerText = `${(data.cena * 0.333).toFixed(2)}`
+        document.getElementById("price375").innerText = `${(data.cena * 0.375).toFixed(2)}`
+        document.getElementById("price417").innerText = `${(data.cena * 0.417).toFixed(2)}`
+        document.getElementById("price500").innerText = `${(data.cena * 0.500).toFixed(2)}`
+        document.getElementById("price585").innerText = `${(data.cena * 0.585).toFixed(2)}`
+        document.getElementById("price750").innerText = `${(data.cena * 0.750).toFixed(2)}`
+        document.getElementById("price900").innerText = `${(data.cena * 0.900).toFixed(2)}`
+        document.getElementById("price999").innerText = `${(data.cena * 0.999).toFixed(2)}`
+    }
+    catch(error){
+        console.error("Something goes wrong: ", error);
+    }
+    
+}
+// getPrice();
+// document.getElementById("btn1").onclick = getPrice;
