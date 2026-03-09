0ac4b59cf670b217fe9845d024e0fd91169af74e
PhorZazek <adversary.jj@gmail.com>
2026-03-09 20:33:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,24 +6,31 @@ async function getPrice(){
             throw new Error("HTTP error: " + response.status);
         }
         const data = await response.json();
+        const value1 = document.getElementById("value1");
+        const value2 = document.getElementById("value2");
+        const value3 = document.getElementById("value3");
+        let multiplier;
+
+        if(value1.checked){multiplier = 1}
+        else if(value2.checked){multiplier = 0.9}
+        else if(value3.checked){multiplier = 0.8}
+        else {multiplier = 0}
 
         console.log(data.cena);
-        document.getElementById("price333").innerText = `${(data.cena * 0.333).toFixed(2)}`
-        document.getElementById("price375").innerText = `${(data.cena * 0.375).toFixed(2)}`
-        document.getElementById("price417").innerText = `${(data.cena * 0.417).toFixed(2)}`
-        document.getElementById("price500").innerText = `${(data.cena * 0.500).toFixed(2)}`
-        document.getElementById("price585").innerText = `${(data.cena * 0.585).toFixed(2)}`
-        document.getElementById("price750").innerText = `${(data.cena * 0.750).toFixed(2)}`
-        document.getElementById("price900").innerText = `${(data.cena * 0.900).toFixed(2)}`
-        document.getElementById("price999").innerText = `${(data.cena * 0.999).toFixed(2)}`
+        document.getElementById("price333").innerText = `${(data.cena * 0.333 * multiplier).toFixed(2)}`
+        document.getElementById("price375").innerText = `${(data.cena * 0.375 * multiplier).toFixed(2)}`
+        document.getElementById("price417").innerText = `${(data.cena * 0.417 * multiplier).toFixed(2)}`
+        document.getElementById("price500").innerText = `${(data.cena * 0.500 * multiplier).toFixed(2)}`
+        document.getElementById("price585").innerText = `${(data.cena * 0.585 * multiplier).toFixed(2)}`
+        document.getElementById("price750").innerText = `${(data.cena * 0.750 * multiplier).toFixed(2)}`
+        document.getElementById("price900").innerText = `${(data.cena * 0.900 * multiplier).toFixed(2)}`
+        document.getElementById("price999").innerText = `${(data.cena * 0.999 * multiplier).toFixed(2)}`
     }
     catch(error){
         console.error("Something goes wrong: ", error);
     }
     
 }
-// getPrice();
-// document.getElementById("btn1").onclick = getPrice;
 
 
 const menuBtn = document.getElementById('menu-btn');
